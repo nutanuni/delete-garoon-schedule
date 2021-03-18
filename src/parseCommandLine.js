4736158e4c80eafeedfc77777b20f57d374ccdbf
nutanuni <j15213ks@gmail.com>
2021-03-18 12:52:40 +0900

    feat: コマンドライン引数のパース処理 オプション引数にドメイン名と設定ファイルを追加

--- src/parseCommandLine.js
+++ src/parseCommandLine.js
@@ -21,6 +21,12 @@ const setting = {
     'e': {
       alias: ['end', 'end-time'],
       description: '削除するスケジュールの期間の終了を指定します'
+    },
+    'domain': {
+      description: 'ドメイン名を指定します'
+    },
+    'config': {
+      description: 'yaml または json 形式の設定ファイルを指定します'
     }
   },
   usage: 'Usage: $0 --user <userName>',
@@ -28,12 +34,22 @@ const setting = {
   check: argv => {
     if (typeof argv.user !== 'undefined' && argv['all-user'] === true)
       throw new Error(`ERROR: -u <--user> と --all-user は同時に指定できません`);
-    if (typeof argv.user === 'undefined' && argv['all-user'] === false)
+    if (typeof argv.config === 'undefined' && typeof argv.user === 'undefined' && argv['all-user'] === false)
       throw new Error(`ERROR: ユーザーを指定してください --all-user で全てのユーザーを指定できます`);
     return true;
   }
 };
 
+/**
+ * コマンドライン引数のパースの結果を返す
+ * @returns {Object} argv
+ * @returns {Array} argv.user ログイン名の配列
+ * @returns {Boolean} argv.allUser 全てのユーザーを示す真偽値
+ * @returns {string} argv.startTime スケジュールの開始を示す
+ * @returns {string} argv.endTime スケジュールの終了を示す
+ * @returns {string} argv.domain ドメイン名を示す
+ * @returns {string} argv.config 設定ファイルのパスを示す
+ */
 const parseCommandLine = () => {
   const argv = yargs(hideBin(process.argv))
     .options(setting.options)
@@ -47,10 +63,12 @@ const parseCommandLine = () => {
     .argv;
 
   return {
-    user: argv.user.split(','),
+    user: typeof argv.user === 'undefined' ? undefined : argv.user.split(','),
     allUser: argv['all-user'],
     startTime: argv['start-time'],
-    endTime: argv['end-time']
+    endTime: argv['end-time'],
+    domain: argv.domain,
+    config: argv.config
   };
 };
 
