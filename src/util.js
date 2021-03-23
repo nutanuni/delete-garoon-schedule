0045d72b37d745b4794fa5ceb596ffe4436125c2
nutanuni <j15213ks@gmail.com>
2021-03-23 18:13:28 +0900

    feat: getExtension関数追加 / jsdoc追加

--- src/util.js
+++ src/util.js
@@ -1,7 +1,23 @@
+import { extname } from 'path';
+
+/**
+ * affix文字列の先頭と末尾にstrを追加した文字列を返す
+ * @param {*} str 
+ * @param {*} affix 
+ * @returns {string}
+ */
 const appendAffix = (str, affix) => {
   return `${affix}${str}${affix}`;
 };
 
+/**
+ * ファイルパスを受け取り、ファイルの拡張子を取得する
+ * @param {string} filePath 
+ * @returns {string}
+ */
+const getExtension = filePath => extname(filePath);
+
 export {
-  appendAffix
+  appendAffix,
+  getExtension
 };
